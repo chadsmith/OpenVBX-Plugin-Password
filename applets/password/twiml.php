c1065a6a0f58ce71d9bd249b623698f03037b857
Chad Smith <chad@nospam.me>
2011-10-12 10:40:58 -0500

    Update for OpenVBX 1.1

--- applets/password/twiml.php
+++ applets/password/twiml.php
@@ -1,17 +1,18 @@
 <?php
 define('PASSWORD_COOKIE', 'password-' . AppletInstance::getInstanceId());
 
+$ci =& get_instance();
 $prompt = AppletInstance::getAudioSpeechPickerValue('prompt');
 $password = AppletInstance::getValue('password');
 $incorrect = AppletInstance::getAudioSpeechPickerValue('incorrect');
 $retries = AppletInstance::getValue('retries', 0);
-$digits = isset($_REQUEST['Digits']) ? $_REQUEST['Digits'] : false;
+$digits = clean_digits($ci->input->get_post('Digits'));
 
 $state = array(
     'retries' => 0
 );
 
-$response = new Response();
+$response = new TwimlResponse;
 
 if(isset($_COOKIE[PASSWORD_COOKIE])) {
 	$state = json_decode(str_replace(', $Version=0', '', $_COOKIE[PASSWORD_COOKIE]), true);
@@ -20,35 +21,52 @@ if(isset($_COOKIE[PASSWORD_COOKIE])) {
 }
 
 if($digits !== false) {
-    if($password !== $digits) {
-        $state['retries']++;
-        if($state['retries'] > $retries) {
+	if($password !== $digits) {
+		$state['retries']++;
+		if($state['retries'] > $retries) {
 			setcookie(PASSWORD_COOKIE);
 			$next = AppletInstance::getDropZoneUrl('fail');
 			if(!empty($next))
-				$response->addRedirect($next);
-			$response->Respond();
+				$response->redirect($next);
+			$response->respond();
 			die;
-        }
-        $verb = AudioSpeechPickerWidget::getVerbForValue($incorrect, new Say('Incorrect password.'));
-        $response->append($verb);
-        $response->addRedirect();
-    }
-    else {
+		}
+		if($incorrect) {
+			AudioSpeechPickerWidget::setVerbForValue($incorrect, $response);
+			$response->redirect();
+		}
+		else {			 
+			$response->say('Incorrect password.', array(
+				'voice' => $ci->vbx_settings->get('voice', $ci->tenant->id),
+				'voice_language' => $ci->vbx_settings->get('voice_language', $ci->tenant->id)
+			));
+			$response->redirect();
+		}
+	}
+	else {
 		setcookie(PASSWORD_COOKIE);
 		$next = AppletInstance::getDropZoneUrl('next');
 		if(!empty($next))
-			$response->addRedirect($next);
-		$response->Respond();
+			$response->redirect($next);
+		$response->respond();
 		die;
-    }
+	}
 }
 else {
-    $gather = $response->addGather(array('numDigits' => strlen($password)));
-    $verb = AudioSpeechPickerWidget::getVerbForValue($prompt, new Say('Please enter the '.strlen($password).' digit password.'));
-    $gather->append($verb);
+	$gather = $response->gather(array('numDigits' => strlen($password)));
+	if($prompt) {
+		AudioSpeechPickerWidget::setVerbForValue($prompt, $response);
+		$response->redirect();
+	}
+	else {			 
+		$response->say('Please enter the ' . strlen($password) . ' digit password.', array(
+			'voice' => $ci->vbx_settings->get('voice', $ci->tenant->id),
+			'voice_language' => $ci->vbx_settings->get('voice_language', $ci->tenant->id)
+		));
+		$response->redirect();
+	}
 }
 
 setcookie(PASSWORD_COOKIE, json_encode($state), time() + (5 * 60));
-$response->Respond();
+$response->respond();
 ?>
